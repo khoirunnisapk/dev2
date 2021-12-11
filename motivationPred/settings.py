f351d55d8fdb5186a642401ba043b1de8f7d785f
khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>
2021-12-12 00:50:16 +0700

    update

--- motivationPred/settings.py
+++ motivationPred/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o$mol-ljgiyfhb=qc$!@(!_p4vcey143vlr=7w*e3_$e^hz3pm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
